20c05ba36f1e3aa552299480771f14e7972ca818
123 <123@123.com>
2023-02-09 21:11:03 +0300

    Changed Program.cs and added Startup.cs

--- WineSales/Program.cs
+++ WineSales/Program.cs
@@ -1,63 +1,25 @@
-using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
-using WineSales.Data;
-using WineSales.Data.Repositories;
-using WineSales.Domain.Interactors;
-using WineSales.Domain.RepositoryInterfaces;
-using WineSales.Domain.Utils;
-
-
-void ConfigureServices(IServiceCollection services)
+namespace ServerING
 {
-    services.AddTransient<ICustomerInteractor, CustomerInteractor>();
-    services.AddTransient<ISaleInteractor, SaleInteractor>();
-    services.AddTransient<ISupplierInteractor, SupplierInteractor>();
-    services.AddTransient<ISupplierWineInteractor, SupplierWineInteractor>();
-    services.AddTransient<IUserInteractor, UserInteractor>();
-    services.AddTransient<IWineInteractor, WineInteractor>();
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-    services.AddTransient<ICustomerRepository, CustomerRepository>();
-    services.AddTransient<ISaleRepository, SaleRepository>();
-    services.AddTransient<ISupplierRepository, SupplierRepository>();
-    services.AddTransient<ISupplierWineRepository, SupplierWineRepository>();
-    services.AddTransient<IUserRepository, UserRepository>();
-    services.AddTransient<IWineRepository, WineRepository>();
-
-    services.AddAutoMapper(typeof(AutoMappingProfile));
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder => {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
 }
-
-
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorPages();
-
-// Configure services.
-ConfigureServices(builder.Services);
-
-// Database connection
-builder.Configuration.AddJsonFile("dbsettings.json");
-builder.Services.AddDbContext<DataBaseContext>(options => options.UseNpgsql(
-      builder.Configuration.GetConnectionString("DefaultConnection")));
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapRazorPages();
-
-app.Run();
---
+++ WineSales/Startup.cs
@@ -0,0 +1,130 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using Microsoft.Extensions.Logging;
+using AutoMapper;
+using System.Text.Json.Serialization;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using WineSales.Data;
+using WineSales.Data.Repositories;
+using WineSales.Domain.Interactors;
+using WineSales.Domain.RepositoryInterfaces;
+using WineSales.Domain.Utils;
+using System.Collections.Generic;
+using Microsoft.OpenApi.Models;
+
+namespace ServerING
+{
+    public class Startup
+    {
+
+        private IConfigurationRoot _configuration;
+
+        public Startup(IWebHostEnvironment hostEnv)
+        {
+            _configuration = new ConfigurationBuilder().SetBasePath(hostEnv.ContentRootPath).AddJsonFile("dbsettings.json").Build();
+        }
+
+        // This method gets called by the runtime
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            // Connect to DB
+            var provider = _configuration["Database"];
+
+            services.AddDbContext<DataBaseContext>(
+                options => _ = provider switch
+                {
+                    "Postgres" => options.UseNpgsql(
+                        _configuration.GetConnectionString("DefaultConnection")
+                        ),
+                    _ => throw new Exception($"Unsupported provider: {provider}")
+                }
+            );
+
+            services.AddTransient<ICustomerInteractor, CustomerInteractor>();
+            services.AddTransient<ISaleInteractor, SaleInteractor>();
+            services.AddTransient<ISupplierInteractor, SupplierInteractor>();
+            services.AddTransient<ISupplierWineInteractor, SupplierWineInteractor>();
+            services.AddTransient<IUserInteractor, UserInteractor>();
+            services.AddTransient<IWineInteractor, WineInteractor>();
+
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<ISaleRepository, SaleRepository>();
+            services.AddTransient<ISupplierRepository, SupplierRepository>();
+            services.AddTransient<ISupplierWineRepository, SupplierWineRepository>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IWineRepository, WineRepository>();
+
+            // MVC
+            services.AddControllers().AddJsonOptions(o =>
+            {
+                var enumConverter = new JsonStringEnumConverter();
+                o.JsonSerializerOptions.Converters.Add(enumConverter);
+            }); // add string enum
+            services.AddEndpointsApiExplorer();
+
+            // Swagger
+            services.AddSwaggerGen();
+
+            // Admin Page
+            //services.AddCoreAdmin();
+
+            // AutoMapper
+            services.AddAutoMapper(typeof(AutoMappingProfile));
+
+            // DTO converters
+            //services.AddDtoConverters(); // self
+
+            // CORS
+            services.AddCors(options => {
+                options.AddPolicy(name: "MyPolicy",
+                    policy => {
+                        policy
+                            .WithOrigins("*")
+                            .WithHeaders("*")
+                            .WithMethods("*");
+                    });
+            });
+        }
+
+        // This method gets called by the runtime
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            if (env.IsDevelopment())
+            {
+                // app.UseSwagger();
+                app.UseSwagger(c => {
+                    c.RouteTemplate = "/api/v1/swagger/{documentName}/swagger.json";
+                });
+                app.UseSwaggerUI(c => {
+                    //Notice the lack of / making it relative
+                    c.SwaggerEndpoint("swagger/v1/swagger.json", "My API V1");
+                    //This is the reverse proxy address
+                    c.RoutePrefix = "api/v1";
+                });
+                app.UseDeveloperExceptionPage();
+            }
+
+            // Authentication
+            // app.UseAuthentication();
+            app.UseRouting();
+            app.UseAuthorization();
+            // app.UseHttpsRedirection();
+
+            app.UseCors();
+
+            app.UseStaticFiles(); // стили для админки
+            //app.UseCoreAdminCustomUrl("admin");
+
+            app.UseEndpoints(endpoints => {
+                endpoints.MapControllers();     // нет определенных маршрутов
+            });
+        }
+    }
+}
