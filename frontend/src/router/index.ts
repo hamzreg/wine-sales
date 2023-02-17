97c291fe181b48d79718e09392c128730bd834ae
Kishov Gadzhi <ns_valet@mail.ru>
2023-02-17 17:48:39 +0300

    test page added

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import TestView from '../views/TestView.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'home',
     component: HomeView
+  },
+
+  {
+    path: '/test',
+    name: 'test',
+    component: TestView
   },
 ]
 
---
+++ frontend/src/views/TestView.vue
@@ -0,0 +1,28 @@
+<template>
+    <HomeBackground>
+    </HomeBackground>
+</template>
+  
+  
+<script lang="ts">
+import { defineComponent } from "vue";
+import HomeBackground from "@/components/HomeBackground.vue"
+
+export default defineComponent({
+setup() {
+    // auth.logout()
+},
+components: {
+    HomeBackground
+},
+});
+</script>
+
+
+<style scoped>
+.logo {
+position: absolute;
+left: 35%;
+bottom: 0%;
+}
+</style>
